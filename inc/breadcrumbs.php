e876f2f3eae28f318a782810e793c44871b179d7
raidho-mx <miranda@raidho.mx>
2015-10-27 16:13:30 -0600

    Best Breadcrumbs in da world

--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -1,13 +1,17 @@
-<?php 
+<?php
 
+// HOME
+	if(is_front_page()) {
 
-	if(is_front_page()) {										// HOME
-		if(get_field('on_bc', 'option')){ 						// If Override ?>
+		// If Override
+		if(get_field('on_bc', 'option')){ ?>
 			<span class="text">
 				<?php the_field('override_bc', 'option') ?>
-			</div><?php 
+			</div><?php
 
-		} else {												// Else 
+
+// ELSE Mostrar Horarios
+		} else {
 
 			$dia = date_i18n('l');
 			$hora = date_i18n('H:i');
@@ -42,135 +46,137 @@
 
 		}
 
-	} elseif(is_404()) { 										// 404 ?>
-	
+// 404
+	} elseif(is_404()) { 										 ?>
+
 		404: Lo que buscas no existe. <span class="text">
 		<a href="<?php echo home_url(); ?>" title="3 Museos">Volver a Inicio</a><?php
 
 
-	} else {													// Breadcrumbs 
+
+
+// Breadcrumbs
+	} else {
 		echo 'Estas en:&nbsp; ';
 
-		if(is_singular('post')) : 								// Single Exposiciones ?>
+	// Single Exposiciones
+		if(is_singular('post')) : ?>
 
-				<a href="<?php echo home_url('/exposiciones'); ?>" title="3 Museos">Exposiciones</a> &nbsp;&rsaquo;&nbsp;
-				<?php if(get_field('expo_status') == 'Permanente') { ?>
-					<a href="<?php echo home_url('/exposiciones'); ?>" title="Exposiciones Actuales">Permanentes</a> &nbsp;&rsaquo;&nbsp; 
-				<?php } else { ?>
-					<a href="<?php echo home_url('/exposiciones/exposiciones-temporales'); ?>" title="Exposiciones Temporales">Temporales</a> &nbsp;&rsaquo;&nbsp; 
-				<?php } ?><span class="text">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php 
-
-
-
-
-		elseif(is_singular('videos')) : 						// Single Videos ?>
-
-				<a href="<?php echo home_url('/recursos'); ?>" title="Recursos">Recursos</a> &nbsp;&rsaquo;&nbsp;
-				<a href="<?php echo home_url('/recursos/videos'); ?>" title="Videos">Videos</a> &nbsp;&rsaquo;&nbsp; 
-				<span class="text">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php 
+			<a href="<?php echo home_url('/exposiciones'); ?>" title="3 Museos">Exposiciones</a> &nbsp;&rsaquo;&nbsp;
+			<?php if(get_field('expo_status') == 'Permanente') { ?>
+				<a href="<?php echo home_url('/exposiciones'); ?>" title="Exposiciones Actuales">Permanentes</a> &nbsp;&rsaquo;&nbsp;
+			<?php } else { ?>
+				<a href="<?php echo home_url('/exposiciones/exposiciones-temporales'); ?>" title="Exposiciones Temporales">Temporales</a> &nbsp;&rsaquo;&nbsp;
+			<?php } ?><span class="text">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php
 
 
+	// Single Videos
+		elseif(is_singular('videos')) : ?>
+
+			<a href="<?php echo home_url('/recursos'); ?>" title="Recursos">Recursos</a> &nbsp;&rsaquo;&nbsp;
+			<a href="<?php echo home_url('/recursos/videos'); ?>" title="Videos">Videos</a> &nbsp;&rsaquo;&nbsp;
+			<span class="text">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php
 
 
+	// Single Piezas
+		elseif(is_singular('pieza')) :
 
-		elseif(is_singular('pieza')) :							// Single Piezas 
+			$cTerm = get_the_terms($post->ID, 'coleccion');
+			$termLink = get_term_link($cTerm[0]->name, 'coleccion');
 
-				$cTerm = get_the_terms($post->ID, 'coleccion');
-				$termLink = get_term_link($cTerm[0]->name, 'coleccion'); 
+		// get parent term
+			$parent = get_term_by( 'id', $cTerm[0]->parent, 'coleccion' );
+			$parentLink = get_term_link($parent->name, 'coleccion'); ?>
 
-				$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); // get current term
-				$parent = get_term($term->parent, get_query_var('taxonomy') ); // get parent term
-				$parentLink = get_term_link($parent->name, 'coleccion'); 
+			<a href="<?php echo esc_url(home_url('/coleccion/mexico-antiguo/')); ?>">Colecciones</a> &nbsp;&rsaquo;&nbsp;<?php
 
-				// echo $term->name;
-				// echo $cTerm->name;
+		// Has Parent
+			if($parent) { ?>
+				<a href="<?php echo esc_url( $parentLink ); ?>"><?php echo $parent->name; ?></a> &nbsp;&rsaquo;&nbsp;
+				<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm[0]->name; ?></a> &nbsp;&rsaquo;&nbsp;<?php
 
-				if(($parent->term_id!="") ) {					// Has Parent ?>
-					<a href="<?php echo esc_url( $parentLink ); ?>"><?php echo $parent->name; ?></a> &nbsp;&rsaquo;&nbsp;
-					<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm[0]->name; ?></a> &nbsp;&rsaquo;&nbsp;<?php 
-				} else { 										// Is Parent ?>
-					<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm[0]->name; ?></a> &nbsp;&rsaquo;&nbsp;<?php 
-				} ?>
-				<span class="text">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php
+		// Is Parent
+			} else { ?>
+				<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm[0]->name; ?></a> &nbsp;&rsaquo;&nbsp;<?php
+			} ?>
+
+			<span class="text">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php
 
 
-			
-		elseif(is_singular('noticias') or is_singular('eventos')) :							// Single Noticias / Eventos 
+	// Single Noticias / Eventos
+		elseif(is_singular('noticias') or is_singular('eventos')) :
 			$pType = get_post_type(get_the_ID());
 			$obj = get_post_type_object($pType); ?>
 
 			<a href="<?php echo get_post_type_archive_link( $pType ); ?>"><?php echo $obj->labels->singular_name; ?></a> &nbsp;&rsaquo;&nbsp;<span class="text">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php 
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php
 
 
-
-		elseif(is_page()) : 										// Pages 
+	// Pages
+		elseif(is_page()) :
 			// echo wp_get_post_parent_id(get_the_ID());
 
-			if(wp_get_post_parent_id(get_the_ID())) { 
+			if(wp_get_post_parent_id(get_the_ID())) {
 			$pID = wp_get_post_parent_id(get_the_ID());  ?>
-		
+
 			<a href="<?php echo get_the_permalink($pID); ?>" title="-"><?php echo get_the_title( $pID ); ?></a> &nbsp;&rsaquo;&nbsp; <span class="text">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php 	
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php
 
 
-			} else { 												// Is Parent ?>
-	
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><span class="text"><?php
-	 	
-	 		} 
- 
-
-	 	elseif(is_archive() && is_tax()): 							// Taxonomy Archive
-
-			$cTerm = get_the_terms($post->ID, 'coleccion');
-			$termLink = get_term_link($cTerm[0]->name, 'coleccion'); 
-
-			$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); // get current term
-			$parent = get_term($term->parent, get_query_var('taxonomy') ); // get parent term
-			$parentLink = get_term_link($parent->name, 'coleccion'); 
-
-			if(($parent->term_id!="") ) {	// Has Parent ?>
-
-				<a href="<?php echo esc_url( $parentLink ); ?>"><?php echo $parent->name; ?></a> &nbsp;&rsaquo;&nbsp;
-				<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm[0]->name; ?></a> <?php 
-
+	// Is Parent
 			} else { ?>
-				<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm[0]->name; ?></a> <?php 
-			} 
-
-			echo '<span class="text">';
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><span class="text"><?php
+			}
 
 
-	 	elseif(is_archive()) :							 		// Archive
+	// Taxonomy Archive
+		elseif(is_archive() && is_tax()):
+
+			global $wp_query;
+			$cTerm = $wp_query->get_queried_object();
+			$termLink = get_term_link($cTerm->name, 'coleccion');
+
+		// get parent term
+			$parent = get_term_by( 'id', $cTerm->parent, 'coleccion' );
+			$parentLink = get_term_link($parent->name, 'coleccion'); ?>
+
+			<a href="<?php echo esc_url(home_url('/coleccion/mexico-antiguo/')); ?>">Colecciones</a> &nbsp;&rsaquo;&nbsp;<?php
+
+		// Has Parent
+			if($parent) { ?>
+				<a href="<?php echo esc_url( $parentLink ); ?>"><?php echo $parent->name; ?></a> &nbsp;&rsaquo;&nbsp;
+				<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm->name; ?></a><?php
+
+		// Is Parent
+			} else { ?>
+				<a href="<?php echo esc_url( $termLink ); ?>"><?php echo $cTerm->name; ?></a><?php
+			}
+
+
+	// Archive
+		elseif(is_archive()) :
 
 			$obj = get_post_type_object(get_post_type()); ?>
 			<a href="<?php get_post_type_archive_link( get_post_type() ); ?>"><?php echo $obj->labels->name; ?></a><span class="text"><?php
-		
 
-		elseif(is_search()) : 									// Search ?>
+
+	// Search
+		elseif(is_search()) : ?>
 
 			Busqueda. <span class="text">
-			<a href="<?php echo home_url(); ?>" title="3 Museos">Volver a Inicio</a><?php 
+			<a href="<?php echo home_url(); ?>" title="3 Museos">Volver a Inicio</a><?php
 
 
-		else : 									 /*				//  Anything else ?>
-		
-			<a href="<?php echo home_url(); ?>" title="3 Museos">Inicio</a> <?php if(get_title()) { ?> &nbsp;&rsaquo;&nbsp; 
+	//  Anything else
+		else : /*?>
+
+			<a href="<?php echo home_url(); ?>" title="3 Museos">Inicio</a> <?php if(get_title()) { ?> &nbsp;&rsaquo;&nbsp;
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><?php }
 
 */
-		endif; 
-
-
+		endif;
 		echo '</span>';
 	} ?>
-
-
-
-
-
-
