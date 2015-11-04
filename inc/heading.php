6578182375323224a20960ad71f611186d0b4b31
raidho-mx <miranda@raidho.mx>
2015-11-04 17:28:55 -0600

    Genero entre Eventos y Noticias.

--- inc/heading.php
+++ inc/heading.php
@@ -3,18 +3,26 @@
 			<?php
 			if(is_post_type_archive('noticias')) {
 				echo '<p>Todas las </p>';
-			} else {
+			} elseif(is_post_type_archive('eventos'))  {
 				echo '<p>Todos los </p>';
 			} ?>
 			<h1><?php
-
 				$obj = get_post_type_object(get_post_type());
 
 				if(is_archive()) {
 					echo $obj->labels->name;
 				} else {
 					the_title();
-				} ?></h1>
-			<?php // if(get_field('kicker')) { echo '<p>'. get_field('kicker') .'</p>'; } ?>
+				} ?>
+			</h1><?php
+
+
+			if(is_page()) {
+				if(get_field('kicker')) {
+					echo '<p>'. get_field('kicker') .'</p>';
+				}
+			} ?>
+
+
 		</div>
 	</div>
