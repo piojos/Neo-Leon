aabc30f34a87a062c375bc214f08b351d0f98ea9
raidho-mx <miranda@raidho.mx>
2015-11-10 11:22:07 -0600

    Tabs as a Module

--- inc/pgwd/tabs.php
+++ inc/pgwd/tabs.php
@@ -1,15 +1,18 @@
+<?php
+
+	$bgImg = get_sub_field('bg-img');
+	$bgImgUrl = wp_get_attachment_image_src( $bgImg, 'larger' ); ?>
+
 <div class="menu">
-	<wrap style="background-image: url(<?php
-		$bgImg = get_sub_field('bg-img');
-		$bgImgUrl = wp_get_attachment_image_src( $bgImg, 'larger' );
-		echo $bgImgUrl[0]; ?>)"><?php
+	<wrap style="background-image: url(<?php echo $bgImgUrl[0]; ?>)"><?php
 
 		if(get_sub_field('title')){
 			echo '<h1>'.get_sub_field('title').'</h1>';
 		} ?>
+
 		<ul class="tabs FU_B"><?php
 			$a = 1;
-			while ( have_rows('groups') ) : the_row();
+			while ( have_rows('repeater') ) : the_row();
 				echo '<li';
 				if($a == 1){
 					echo ' class="current"';
@@ -18,20 +21,27 @@
 			endwhile; ?>
 		</ul>
 
-		<ul class="content"><?php
+
+		<div class="tContent"><?php
 			$b = 1;
-			while ( have_rows('groups') ) : the_row(); ?>
-				<li class="<?php
+			while ( have_rows('repeater') ) : the_row();
 				if (get_sub_field('2_cols')) {
-					echo 'cols_2 ';
+					$classes = 'cols_2';
 				}
 				if($b == 1){
-					echo 'current';
-				} ?>" id="tab-<?php echo $b++; ?>" >
-					<?php the_sub_field('list'); ?>
-				</li><?php
+					$shhi = 'style="display:inline"';
+				} else {
+					$shhi = 'style="display:none"';
+				}?>
+				<div class="<?php echo $classes; $classes = ''; ?>" id="tab-<?php echo $b++; ?>" <?php echo $shhi; ?>><?php
+					if(get_sub_field('list')){
+						the_sub_field('list');
+					} else {
+						the_sub_field('content');
+					} ?>
+				</div><?php
 			endwhile; ?>
-		</ul>
+		</div>
 
 	</wrap>
 </div>
@@ -41,10 +51,12 @@
 	$(document).ready(function() {
 		$(".tabs a").click(function(event) {
 			event.preventDefault();
+
 			$(this).parent().addClass("current");
 			$(this).parent().siblings().removeClass("current");
 			var tab = $(this).attr("href");
-			$(".content li").not(tab).css("display", "none");
+			console.log(tab);
+			$(".tContent div").not(tab).css("display", "none");
 			$(tab).fadeIn();
 		});
 	});
