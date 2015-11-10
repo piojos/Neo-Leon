2432c9b6cf9a50cd3d16e964e35740e9c7e9e854
raidho-mx <miranda@raidho.mx>
2015-11-10 11:13:09 -0600

    Added description option

--- inc/single-video.php
+++ inc/single-video.php
@@ -6,21 +6,33 @@
 		<div class="contain">
 			<wrap>
 
-			<div class="video-container video">
-				<?php if(get_field('slct_src') == 'upload') { ?>
-				
-					<video width="640" height="360" <?php // width="100%" 
+			<div<?php
+			if(!get_field('description')){
+				echo ' class="video-container video"';
+			} else {
+				echo ' class="video has-caption"';
+			}?>><?php
+
+				if(get_field('slct_src') == 'upload') { ?>
+
+					<video width="640" height="360" <?php // width="100%"
 						$bgImg = get_post_thumbnail_id( $post_id );
-						$img_large = wp_get_attachment_image_src($bgImg, 'large'); 
+						$img_large = wp_get_attachment_image_src($bgImg, 'large');
 						if('$bgImg') {
 							echo 'poster="'.$img_large[0].'"';
 						}  ?> controls="controls" preload="none">
-					    <source type="video/mp4" src="<?php the_field('src_ul'); ?>" />
-					</video>
+						<source type="video/mp4" src="<?php the_field('src_ul'); ?>" />
+					</video><?php
 
-				<?php } elseif(get_field('slct_src') == 'youtube'){ 
-					echo '<iframe width="853" height="480" src="https://www.youtube.com/embed/'.get_field('src_yt').'" frameborder="0" allowfullscreen></iframe>'; ?>
-				<?php } ?>
+				} elseif(get_field('slct_src') == 'youtube'){
+					echo '<iframe width="853" height="480" src="https://www.youtube.com/embed/'.get_field('src_yt').'" frameborder="0" allowfullscreen></iframe>';
+				}
+
+				if(get_field('description')){ ?>
+					<div class="caption">
+						<?php the_field('description'); ?>
+					</div><?php
+				} ?>
 			</div>
 
 			</wrap>
@@ -31,11 +43,11 @@
 	<?php echo get_template_part('inc/more_news'); ?>
 
 <script>
-	
+
 	$('.video-container video').mediaelementplayer({
 		features: ['playpause', 'current', 'progress', 'duration'],
 	});
-	
+
 	$(".video-container").fitVids();
 
 </script>
