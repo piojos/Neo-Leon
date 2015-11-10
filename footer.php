08f9432697d1c1a08d9a719d517035802711d045
raidho-mx <miranda@raidho.mx>
2015-11-10 11:01:49 -0600

    Added Typekit & Link to contact

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
 				include 'inc/newsletter.php';
 			} ?>
 
-<?php 
-		if( have_rows('f_menu', 'option') ) : 
+<?php
+		if( have_rows('f_menu', 'option') ) :
 			echo '<ul class="menu">';
 			while( have_rows('f_menu', 'option') ): the_row(); ?>
 
@@ -18,25 +18,25 @@
 					</a>
 				</li>
 
-<?php 		endwhile; 
+<?php 		endwhile;
 			echo '</ul>';
 		endif;  ?>
 
 				<ul class="mobile">
-					<a href="#" class="switch">Ver sitio completo</a><?php 
+					<a href="<?php echo esc_url(home_url('/contacto/')); ?>" class="switch">Contacto</a><?php
 					while ( have_rows('redes', 'option') ) : the_row();
 
-						if(get_sub_field('channel') == 'otro') { 
-							echo '<style> footer .share.otro:hover { background-image: url('.get_sub_field('ic_color').') !important; } </style>'; 
+						if(get_sub_field('channel') == 'otro') {
+							echo '<style> footer .share.otro:hover { background-image: url('.get_sub_field('ic_color').') !important; } </style>';
 						}
 
 						echo '<a href="';
-						if(get_sub_field('channel') == 'otro') { 
-							echo get_sub_field('url').'" style="background-image: url('.get_sub_field('ic_gray').');'; 
-						} elseif(get_sub_field('channel') == 'tumblr') { 
-							echo 'http://'.get_sub_field('usuario').'tumblr.com/'; 
-						} else { 
-							echo 'http://www.'.get_sub_field('channel').'.com/'.get_sub_field('usuario'); 
+						if(get_sub_field('channel') == 'otro') {
+							echo get_sub_field('url').'" style="background-image: url('.get_sub_field('ic_gray').');';
+						} elseif(get_sub_field('channel') == 'tumblr') {
+							echo 'http://'.get_sub_field('usuario').'.tumblr.com/';
+						} else {
+							echo 'http://www.'.get_sub_field('channel').'.com/'.get_sub_field('usuario');
 						}
 
 						echo '" class="share '.get_sub_field('channel').'"></a>';
@@ -45,7 +45,7 @@
 				</ul>
 				<div class="copyright">
 					<p>Todos los derechos reservados 3 Museos &copy; <?php echo current_time('Y'); ?></p>
-					<p><a href="http://archivo.nl.gob.mx/?P=transparencia_mhm">Transparencia</a> | 
+					<p><a href="http://archivo.nl.gob.mx/?P=transparencia_mhm">Transparencia</a> |
 					<a href="<?php echo esc_url( home_url( '/privacidad' ) ); ?>">Privacidad</a></p>
 				</div>
 			</wrap>
@@ -56,7 +56,7 @@
 				<div class="associates">
 					<h2 class="FU_B"><?php the_field('f_quote', 'option'); ?></h2>
 
-<?php 			while( have_rows('logos', 'option') ): the_row(); 
+<?php 			while( have_rows('logos', 'option') ): the_row();
 					$logo = get_sub_field('logo');
 					$get = wp_get_attachment_image_src( $logo, 'medium' ); ?>
 
@@ -68,7 +68,7 @@
 
 				</div>
 				<div class="certificates">
-<?php 			while( have_rows('certs', 'option') ): the_row(); 
+<?php 			while( have_rows('certs', 'option') ): the_row();
 					$cert = get_sub_field('logo');
 					$get = wp_get_attachment_image_src( $cert, 'medium' ); ?>
 
@@ -94,7 +94,7 @@
 		});
 	});
 	$( "a.search-btn, a.close-search" ).click(function() {
-		$( "header" ).toggleClass( "show-search" );          
+		$( "header" ).toggleClass( "show-search" );
 	});
 
 	$(document).ready(function() {
--- header.php
+++ header.php
@@ -5,6 +5,17 @@
 
 	<title><?php wp_title(''); ?></title>
 
+	<script>
+		(function(d) {
+			var config = {
+				kitId: 'iik6myc',
+				scriptTimeout: 3000,
+				async: true
+			},
+			h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+		})(document);
+	</script>
+
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
 	<link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/extra.css">
 
