589721ba8a2d8aa7a6dc4ba1162c5508ece0bee9
raidho-mx <miranda@raidho.mx>
2015-11-10 11:23:02 -0600

    Added archive loop

--- inc/loop.php
+++ inc/loop.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 	// $terms = get_the_terms( $post->ID, 'coleccion' );
-	// if ( $terms && ! is_wp_error( $terms ) ) : 
+	// if ( $terms && ! is_wp_error( $terms ) ) :
 	// 	foreach ( $terms as $term ) {
 	// 		$draught_links = $term->slug;
 	// 	}
@@ -12,9 +12,19 @@
 		$term = $wp_query->get_queried_object();
 		$slug = $term->slug;
 		echo $title;
-	} 
+	}
 
-	if (is_tax('coleccion') && $paged == '0' ) {    
+	if(is_page()){
+		global $post;
+		$pSlug = $post->post_name;
+		query_posts(
+			array(
+				'post_type' => $pSlug,
+				'posts_per_page' => 12,
+				'paged' => get_query_var( 'paged' )
+			)
+		);
+	} elseif (is_tax('coleccion') && $paged == '0' ) {
 		query_posts(
 			array(
 				'posts_per_page' => -1,
@@ -28,23 +38,23 @@
 				),
 				'post_type' => 'pieza'
 			)
-		); 
+		);
 	} elseif (is_page('videos')) {
 		query_posts(
 			array(
 				'posts_per_page' => -1,
 				'post_type' => 'videos'
 			)
-		); 
+		);
 	}
 
 
 
 	if ( have_posts() ) {
 		echo '<ul class="masonry cards">';
-		while ( have_posts() ) { the_post(); 
-			echo get_template_part('inc/cards'); 
-		} 
+		while ( have_posts() ) { the_post();
+			echo get_template_part('inc/cards');
+		}
 		echo '</ul>';
 
 		if (function_exists("pagination")) {
@@ -53,5 +63,3 @@
 	}
 
 ?>
-
-
