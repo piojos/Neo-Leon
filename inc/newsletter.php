38ccf04c33f84daa7543bd0ab7b89bf739128670
raidho-mx <miranda@raidho.mx>
2016-04-12 18:06:33 -0500

    Added mailchimp

--- inc/newsletter.php
+++ inc/newsletter.php
@@ -3,8 +3,11 @@
 			<p>Suscribete</p>
 			<h3>No te pierdas lo ultimo de 3 Museos</h3>
 		</div>
-		<form>
-			<input type="email" placeholder="Correo">
-			<input type="submit" value="Suscribir">
+
+		<form action="//3museos.us12.list-manage.com/subscribe/post?u=1a8d99285d5319106e605b4df&amp;id=3a72082e88" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Correo" required>
+			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1a8d99285d5319106e605b4df_3a72082e88" tabindex="-1" value=""></div>
+			<div class="clear"><input type="submit" value="Suscribir" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 		</form>
 	</div>
